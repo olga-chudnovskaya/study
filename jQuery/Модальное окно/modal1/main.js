820bf504be8b61c3945dd033ebd26969df511e12
Olga Chudnovskaya <chudnovskaya.frontend@gmail.com>
2021-06-24 10:03:17 +0300

    com1

--- jQuery/Модальное окно/modal1/main.js
+++ jQuery/Модальное окно/modal1/main.js
@@ -1,22 +1,21 @@
 /* Функция закрывает по нажатие на кнопку модальное окно */
 function bclose() {
-	$("#popup_vk").bPopup().close();
+	$("#popup").bPopup().close();
 	return false;
 }
 
 /* Функция показывает модальное окно по нажатию на кнопку */
 function bopen() {
-	$('#popup_vk').bPopup();
+	$('#popup').bPopup();
 	return false;
 }
 
+
 /* Функция отображает модальное окно автоматически после загрузки страницы через одну секунду */
-$( document ).ready(function() {
-	setTimeout(function() {
-        /* Подключение vk виджета с группой*/
-        VK.Widgets.Group("vk_groups_popup", {mode: 0, width: "220", height: "400"}, 144839492);
-        
+//$( document ).ready(function() {
+	//setTimeout(function() {
+                
         /* Вызываем модальное окно */
-        $('#popup_vk').bPopup();	
-	}, 1000);
-});
+        //$('#popup').bPopup();	
+	//}, 1000);
+//});
